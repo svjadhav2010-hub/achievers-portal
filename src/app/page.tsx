5682cf459e8a80f75a11e606808bc0707162646a
Swayam Jadhav <swayamvjadhav2010@gmail.com>
2026-04-15 20:12:56 +0530

    Reviews fetched from google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -751,6 +751,10 @@ export default function HomePage() {
 
             {/* Google review */}
             <div className="testi-card card-lift" style={{ marginTop: 16 }}>
+             {/*<!-- Elfsight Google Reviews | Untitled Google Reviews -->*/} 
+            <script src="https://elfsightcdn.com/platform.js" async></script>
+            <div className="elfsight-app-c95d4d44-5a6b-4869-9716-7f8f8a2cb903" data-elfsight-app-lazy></div>
+            {/* Since we can't render the actual Elfsight widget in this static code, we'll mock up a review card that matches the style of the rest of the site. */}
               <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: 20, paddingBottom: 20, borderBottom: '1px solid rgba(0,0,0,0.06)' }}>
                 <div style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
                   <svg width="20" height="20" viewBox="0 0 24 24"><path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/><path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"/><path fill="#FBBC05" d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"/><path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"/></svg>
